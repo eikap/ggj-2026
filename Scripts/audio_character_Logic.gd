ae1db30d371a33b3f74a5602f178c4fd1bbcbfaf
SimonStevnhoved20 <simon_s_rasmussen@hotmail.com>
2026-01-31 22:18:39 +0100

    Audio updates

--- Scripts/audio_character_Logic.gd
+++ Scripts/audio_character_Logic.gd
@@ -1,14 +1,32 @@
 extends AudioStreamPlayer2D
+@onready var audio_character: AudioStreamPlayer2D = $"."
 @onready var audio_voice: AudioStreamPlayer2D = $Audio_Voice
 @onready var audio_bubbles: AudioStreamPlayer2D = $Audio_Bubbles
 @onready var oxygen_level_test: VSlider = $"Oxygen level test"
+@export var player: Player
 
-var volume = 0
+var fadeOutGate = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if  oxygen_level_test.value < 80 && audio_voice.playing == false:
+	if  player.oxygen < 80 && player.oxygen != 0 && audio_voice.playing == false:
+		audio_voice.volume_db = -80
+		audio_bubbles.volume_db = -80
 		audio_bubbles.play()
 		audio_voice.play()
-	if  oxygen_level_test.value >= 80 && audio_voice.playing == true:
-		audio_bubbles.stop()
-		audio_voice.stop()
+		var fadeIn = get_tree().create_tween()
+		fadeIn.tween_property(audio_voice,"volume_db",0,1)
+		fadeIn.tween_property(audio_bubbles,"volume_db",0,1)
+		fadeOutGate = true
+		#print("play")
+
+	if  (player.oxygen >= 80 || player.oxygen == 0) && audio_voice.playing == true:
+		if fadeOutGate == true:
+			var fadeOut = get_tree().create_tween()
+			fadeOut.tween_property(audio_bubbles,"volume_db",-80,0.5)
+			fadeOut.tween_property(audio_voice,"volume_db",-80,0.5)
+			#print("fadeout start")
+			fadeOutGate = false
+		if audio_bubbles.volume_db == -80:
+				audio_voice.stop()
+				audio_bubbles.stop()
+				#print("sound stopped")				
