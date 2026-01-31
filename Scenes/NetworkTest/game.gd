e2fbbdda6abfb834b3f935a69e1ecfd43ae47f3d
Kasparas Eidukonis <kasparas6@hotmail.com>
2026-01-31 10:13:45 +0100

    Add some network test code

---
+++ Scenes/NetworkTest/game.gd
@@ -0,0 +1,13 @@
+extends Node3D # Or Node2D.
+
+
+
+func _ready():
+	# Preconfigure game.
+	Network.player_loaded.rpc_id(1) # Tell the server that this peer has loaded.
+
+
+# Called only on the server.
+func start_game():
+	# All peers are ready to receive RPCs in this scene.
+	pass
