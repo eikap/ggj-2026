de2e5cab0943847e0a7f25f9bfefda84c047792c
Kasparas Eidukonis <kasparas6@hotmail.com>
2026-02-01 15:03:49 +0100

    Set icon and add lobby

--- Scenes/splash_screen.gd
+++ Scenes/splash_screen.gd
@@ -4,4 +4,4 @@ func _ready():
 	$AnimationPlayer.play("text fade in")
 
 func _on_timer_timeout() -> void:
-	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/NetworkTest/LobbyTest.tscn")
