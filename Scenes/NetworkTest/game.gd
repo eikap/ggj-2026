f347624ab322ca7e77dd5b093f2ae035132a6eab
Kasparas Eidukonis <kasparas6@hotmail.com>
2026-02-01 14:01:52 +0100

    Half of the players get masks

--- Scenes/NetworkTest/game.gd
+++ Scenes/NetworkTest/game.gd
@@ -21,7 +21,7 @@ func _ready():
 # Called only on the server.
 func start_game():
 	var numPlayers = Network.players.size()
-	var maskedPlayers = 1 #max(1, numPlayers - 2)
+	var maskedPlayers = floor(float(numPlayers) / 2.0) #max(1, numPlayers - 2)
 	
 	for i in range(0, maskedPlayers):
 		var playerID = Network.players.keys()[i]
